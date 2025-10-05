263a494ef31a4788c2bc15814a45138b9aa1e77b
Ricardo Cruz <ricardo_cruz@epam.com>
2025-10-05 04:59:23 -0600

    Adding index page

--- src/main/java/com/epam/rd/autocode/spring/project/controller/HomeController.java
+++ src/main/java/com/epam/rd/autocode/spring/project/controller/HomeController.java
@@ -1,12 +1,19 @@
 package com.epam.rd.autocode.spring.project.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+    
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        // Add some sample data to display in the template
+        model.addAttribute("totalBooks", "12,500+");
+        model.addAttribute("totalCustomers", "8,200+");
+        model.addAttribute("totalOrders", "35,000+");
+        
         return "index";
     }
 }
